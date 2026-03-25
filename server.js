734c700ead8da390793e6b9b38be01c345b41148
Green Power Tech Store LLC <gpts.citas@gmail.com>
2026-03-24 23:46:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post("/create-payment-intent", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Backend running on port ${PORT}`);
